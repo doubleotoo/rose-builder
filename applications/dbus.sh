9a25ee2339ce63d1fbb346787b2b637e7f23e7a0
Justin Too <doubleotoo@gmail.com>
2014-01-03 16:04:28 -0800

    (applications) Add dbus

---
+++ applications/dbus.sh
@@ -0,0 +1,74 @@
+#-------------------------------------------------------------------------------
+download_dbus()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      git clone \
+          "rose-dev@rosecompiler1.llnl.gov:rose/c/${application}.git" \
+          "${application}-src" \
+          || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+patch_dbus__rose()
+#-------------------------------------------------------------------------------
+{
+  info "ROSE patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_dbus__generic()
+#-------------------------------------------------------------------------------
+{
+  local autoconf_version="$(autoconf --version | head -1 | awk '{print $NF}' | sed 's/\.//g')"
+
+  if [ "$autoconf_version" -lt 263 ]; then
+    fail "${application} requires Autoconf version >= 2.63"
+  elif [ -z "$(which cmake)" ]; then
+    fail "${application} requires CMake -- please set your \$PATH"
+  fi
+
+  mkdir dbus-build-dir || exit 1
+  cd dbus-build-dir || exit 1
+}
+
+#-------------------------------------------------------------------------------
+configure_dbus__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for ROSE compiler='${rose_compiler}'"
+
+  configure_dbus__generic || exit 1
+
+  set -x
+      CC="$rose_compiler" cmake -G "Unix Makefiles" ../cmake/ || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+configure_dbus__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='gcc'"
+
+  configure_dbus__generic || exit 1
+
+  set -x
+      CC="gcc" cmake -G "Unix Makefiles" ../cmake/ || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+compile_dbus()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application"
+
+  set -x
+      make -j${parallelism}  || exit 1
+  set +x
+}
