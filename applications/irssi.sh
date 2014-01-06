785f4eaf50d741cd11858bc7136942335d919f5a
Justin Too <doubleotoo@gmail.com>
2014-01-06 13:31:57 -0800

    (applications) Add irssi

---
+++ applications/irssi.sh
@@ -0,0 +1,56 @@
+#-------------------------------------------------------------------------------
+download_irssi()
+#-------------------------------------------------------------------------------
+{
+  info "Downloading source code"
+
+  set -x
+      git clone \
+          "rose-dev@rosecompiler1.llnl.gov:rose/c/${application}.git" \
+          "${application}-src" \
+          || exit 1
+      cd "${application}-src/" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+patch_irssi__rose()
+#-------------------------------------------------------------------------------
+{
+  info "ROSE patching not required"
+}
+
+#-------------------------------------------------------------------------------
+configure_irssi__rose()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for ROSE compiler='${CC}'"
+
+  set -x
+      CC="${CC} -rose:C89_only" \
+          ./configure --prefix="$(pwd)/install_tree" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+configure_irssi__gcc()
+#-------------------------------------------------------------------------------
+{
+  info "Configuring application for default compiler='gcc'"
+
+  set -x
+      CC="gcc" \
+          ./configure --prefix="$(pwd)/install_tree" || exit 1
+  set +x
+}
+
+#-------------------------------------------------------------------------------
+compile_irssi()
+#-------------------------------------------------------------------------------
+{
+  info "Compiling application"
+
+  set -x
+      make -j${parallelism}  || exit 1
+  set +x
+}
